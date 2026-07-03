81be3a547ac2a658d0549f2a11e3d4049d53c5f1
Eduard Paul Lakida <eduardpaul@users.noreply.github.com>
2026-07-03 04:46:39 +0200

    fix: channel

--- pkg/core/codec.go
+++ pkg/core/codec.go
@@ -130,8 +130,8 @@ func (c *Codec) Match(remote *Codec) bool {
 	}
 
 	return c.Name == remote.Name &&
-		(c.ClockRate == remote.ClockRate || remote.ClockRate == 0) &&
-		(c.Channels == remote.Channels || remote.Channels == 0)
+		(c.ClockRate == remote.ClockRate || remote.ClockRate == 0 || c.ClockRate == 0) &&
+		(c.Channels == remote.Channels || remote.Channels == 0 || c.Channels == 0)
 }
 
 func UnmarshalCodec(md *sdp.MediaDescription, payloadType string) *Codec {
